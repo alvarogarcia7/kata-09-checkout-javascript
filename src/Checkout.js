088a4f3a31047fe9f49464283ee0d57d8988fc8a
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-05-02 21:59:13 +0200

    green

--- src/Checkout.js
+++ src/Checkout.js
@@ -7,13 +7,16 @@ Checkout.prototype.total = function() {
 };
 
 Checkout.prototype.scan = function(products) {
-	var i, j=0;
+	var i, j=0, unitPrice = 50, multipleUnitsDiscount = 20;
+	if("B" == products){
+		unitPrice = 30;
+	}
 	for (i = products.length - 1; i >= 0; i--) {
-		this.totalAmount += 50;
+		this.totalAmount += unitPrice;
 		j++;
 		if(j%3 == 0){
 			j = 0;
-			this.totalAmount -= 20;
+			this.totalAmount -= multipleUnitsDiscount;
 		}
 	};
 	
