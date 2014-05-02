b0d621db32486aafff7c110b5c45be066fa48b43
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-05-02 20:51:39 +0200

    red: Checkout should exist

---
+++ spec/CheckoutSpec.js
@@ -0,0 +1,9 @@
+describe("Checkout", function(){
+	var checkout;
+	beforeEach(function () {
+		checkout = new Checkout();
+	});
+	it("should exist", function(){
+		expect(checkout).toBeTruthy();
+	});
+});
