5e10607e8ce6aa00ed1e75528f1316bee671635a
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-05-02 21:00:56 +0200

    red: Checkout should be simple pricing with one product.

--- spec/CheckoutSpec.js
+++ spec/CheckoutSpec.js
@@ -11,4 +11,9 @@ describe("Checkout", function(){
 	it("should be zero when there are no items", function(){
 		expect(checkout.total()).toEqual(0);
 	});
+
+	it("should be simple pricing with one product", function(){
+		checkout.scan("A");
+		expect(checkout.total()).toEqual(50);
+	});
 });
