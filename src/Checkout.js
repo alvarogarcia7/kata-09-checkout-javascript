f75911f950f97a752fc5d156c1d3a6eaec66689a
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-05-03 10:00:03 +0200

    refactor: include discounted price as pricing calculation, not hardcoded values

--- src/Checkout.js
+++ src/Checkout.js
@@ -14,24 +14,25 @@ Checkout.prototype.total = function() {
 };
 
 Checkout.prototype.scan = function(products) {
-	var i, j=0, unitPrice, multipleUnitsDiscount = 20;
+	var i, j=0, unitPrice;
 	
 	for (i = products.length - 1; i >= 0; i--) {
 		if(this.productPricing[products[i]]){
 			unitPrice = this.productPricing[products[i]];
 			this.amount[products[i]]++;
 			this.totalAmount += unitPrice;
-		}
 
-		if(this.isDiscountedProduct(products[i])){
-			this.totalAmount -= multipleUnitsDiscount;
+			if(this.isDiscountedProduct(products[i])){
+				this.totalAmount -= this.amount[products[i]]*unitPrice;
+				this.totalAmount += this.productPricing[this.getCompoundName(products[i])];
+				this.amount[products[i]]=0;
+			}	
 		}
 	};
 
 };
 
-Checkout.prototype.isDiscountedProduct = function(product) {
-
+Checkout.prototype.getCompoundName = function(product) {
 	var compoundName = '';
 
 	if(this.amount[product] === 2){
@@ -39,9 +40,14 @@ Checkout.prototype.isDiscountedProduct = function(product) {
 	} else if(this.amount[product] === 3) {
 		compoundName= product+""+product + ""+product;
 	}
+	return compoundName;
+};
+
+Checkout.prototype.isDiscountedProduct = function(product) {
+
+	var compoundName = this.getCompoundName(product);
 
 	if(this.productPricing[compoundName]){
-		this.amount[product] = 0;
 		return true;
 	} else {
 		return false;
