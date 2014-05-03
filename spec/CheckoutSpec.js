321b4ebc0a63e3c3060647a15dcb7c31c1af2a39
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-05-03 10:06:28 +0200

    green: added regression tests

--- spec/CheckoutSpec.js
+++ spec/CheckoutSpec.js
@@ -24,9 +24,37 @@ describe("Checkout", function(){
 		});
 	});
 
-	it("should be simple pricing with one product", function(){
+	describe("applying product B", function(){
+		it("should be simple pricing with one product", function(){
 			checkout = new Checkout({"B":30});
 			checkout.scan("B");
 			expect(checkout.total()).toEqual(30);
 		});
+
+		it("should be simple pricing with multiple products", function(){
+			checkout = new Checkout({"B":30, "BB":45});
+			checkout.scan("BB");
+			expect(checkout.total()).toEqual(45);
+		});
+	});
+
+	describe("applying multiple products", function(){
+		it("should not matter the order of the products", function(){
+			checkout = new Checkout({"A":50, "AAA":130, "B":30});
+			checkout.scan("AA");
+			checkout.scan("B");
+			checkout.scan("A");
+			expect(checkout.total()).toEqual(130+30);
+		});
+
+		it("should increment the price at each scan", function(){
+			checkout = new Checkout({"A":50, "AAA":130, "B":30});
+			checkout.scan("AA");
+			expect(checkout.total()).toEqual(2*50);
+			checkout.scan("B");
+			expect(checkout.total()).toEqual(2*50+30);
+			checkout.scan("A");
+			expect(checkout.total()).toEqual(130+30);
+		});
+	});
 });
